c6c5fbdc335f3282f73b2a82edba0eceef7bc7f3
Nitin <hawkr173@gmail.com>
2026-07-07 15:16:12 +0530

    Refactor renameInScope command to handle current and new variable names with improved validation and feedback

--- src/extension.ts
+++ src/extension.ts
@@ -1,3 +1,4 @@
+import { text } from 'stream/consumers';
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
@@ -14,7 +15,21 @@ export function activate(context: vscode.ExtensionContext) {
 			return;
 		}
 		const scptxt = edtr.document.getText(selection);
-		const nwNm = await vscode.window.showInputBox({
+
+		const oldNm = await vscode.window.showInputBox({ // Handling Current name
+			prompt: 'Enter the Variable name ',
+			placeHolder: 'var',
+			validateInput: (text) => {
+				return text.length === 0 ? 'Variable name cannot be Empty.' : null;
+			}
+		});
+
+		if (!oldNm){
+			// vscode.window.showInformationMessage('Rename cancelled.');
+			return;
+		}
+
+		const nwNm = await vscode.window.showInputBox({ // Handle Updated name.
 			prompt: 'Enter new name',
 			placeHolder: 'Rename',
 			validateInput: (text) => {
@@ -22,16 +37,38 @@ export function activate(context: vscode.ExtensionContext) {
 			}
 		});
 		if (!nwNm){
-			vscode.window.showInformationMessage('Rename Cancelled.');
+			// vscode.window.showInformationMessage('Rename Cancelled.');
 			return;
 		};
 
-		const stLn = selection.start.line + 1;
-		const edLn = selection.end.line + 1;
+		// Regex Logic Implementation
+		const escRgx = (string: string) => {
+			return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+		};
 
-		vscode.window.showInformationMessage(
-			`Success! Scope: Lines ${stLn} to ${edLn}. New NameL "${nwNm}.`
-		);
+		const rgx = new RegExp(`\\b${escRgx(oldNm)}\\b`, 'g');
+		if (!rgx.test(scptxt)) {
+			vscode.window.showWarningMessage(`"${oldNm}" was not found in the selected scope.`);
+			return;
+		}
+
+		rgx.lastIndex = 0;
+		const nwScpTxt = scptxt.replace(rgx, nwNm);
+
+		const scs = await edtr.edit(edtBldr => {
+			edtBldr.replace(selection, nwScpTxt);
+		});
+
+		if (scs){
+			const stLn = selection.start.line + 1;
+			const edLn = selection.end.line + 1;
+
+			vscode.window.showInformationMessage(
+				`Success! Scope: Lines ${stLn} to ${edLn}. New NameL "${nwNm}.`
+			);
+		} else {
+			vscode.window.showErrorMessage('Failed to apply changes');
+		}
 	});
 	context.subscriptions.push(dpsable);
 }
